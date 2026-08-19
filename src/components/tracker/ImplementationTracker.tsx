9c1fb1ba13838996ab0b738691918f827467d6ae
ArSi <silvan@arsija.net>
2026-08-19 17:27:03 +0200

    perf(tracker): improve tracker list render performance

--- src/components/tracker/ImplementationTracker.tsx
+++ src/components/tracker/ImplementationTracker.tsx
@@ -247,7 +247,7 @@ export default function ImplementationTracker() {
           return (
             <div
               key={className}
-              className="rounded-xl border border-teal-200/30 dark:border-white/10 bg-white/60 dark:bg-white/[0.03] overflow-hidden transition-all"
+              className="rounded-xl border border-teal-200/30 dark:border-white/10 bg-white/60 dark:bg-white/[0.03] overflow-hidden transition-all [content-visibility:auto] [contain-intrinsic-size:auto_500px]"
             >
               <button
                 onClick={() => toggleExpand(className)}
